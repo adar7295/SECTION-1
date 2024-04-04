5bec7f1c5a5aedde33e625c9f2820a565528da67
adar7295 <ssrivastav7295@gmail.com>
2024-04-04 14:48:12 +0530

    filter

--- map_filter.js
+++ map_filter.js
@@ -1,3 +1,4 @@
+//maps
 const nums =[4,8,3,5,6];
 
 const newNums=[];
@@ -17,3 +18,20 @@ console.log(newArr1);
 const lower =['ADARSH','ABHAY','ARPIT','SHAURYA','AVIJJEET','VIKAS','PRABAL','ABHISHEK'];
 const newArr2=lower.map( (b) => { return b.toLowerCase()});
 console.log(newArr2);
+
+
+//filters
+
+const evenNums = nums.filter((n) => {return n%2===0});
+console.log(evenNums);
+
+const prices = [530,990,2880,3200,800,120];
+const ranges = prices.filter((n)=>{ return n>800 && n<1500});
+console.log(ranges);
+
+const nam= names.filter((n)=>{ return n.length<=5});
+console.log(nam);
+
+const strPrice = ['₹56.90','₹780.892','₹190.6','₹90.8','₹120.9'];
+const intPrice = strPrice.map((n)=>{return parseInt(n.slice(1))});
+console.log(intPrice);
